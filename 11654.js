b77abbf5d0d4c8a465dd0c15502a3d7a99545196
sounimul <sounimul12@gmail.com>
2023-06-16 23:25:49 +0900

    11654 solution

---
+++ 11654.js
@@ -0,0 +1,28 @@
+/* 11654 아스키 코드 */
+
+/* input.txt
+   A
+*/
+
+/* input.txt
+   C
+*/
+
+/* input.txt
+   0
+*/
+
+/* input.txt
+   9
+*/
+
+/* input.txt
+   a
+*/
+
+/* input.txt
+   z
+*/
+
+
+console.log(require('fs').readFileSync('input.txt').toString().charCodeAt());
